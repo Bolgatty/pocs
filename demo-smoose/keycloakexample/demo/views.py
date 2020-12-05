21d1a8a59609ac5de9032d917d7fc80f3d6f1314
Gokul kartha <kartha.gokul@gmail.com>
2020-12-05 22:50:12 +0000

    changes

--- demo-smoose/keycloakexample/demo/views.py
+++ demo-smoose/keycloakexample/demo/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-
+from django.shortcuts import render_to_response
 
 def index(request):
     return HttpResponse('Welcome to Smoose Portal <a href="/secure">Login To Keycloak</a>')
 
 @login_required
 def secure(request):
-    return HttpResponse('Logout from KeyCloak. <a href="/openid/logout">Logout</a>')  
+    #return HttpResponse('Logout from KeyCloak. <a href="/openid/logout">Logout</a>')  
+    return render_to_response("demo/home.html", {"userinfo": request.session['userinfo'] if 'userinfo' in request.session.keys() else None})
